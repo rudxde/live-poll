594da714053cf236603614c29f6768b806cab4ea
rudxde <kj4derechte@gmail.com>
2021-02-13 22:45:42 +0100

    refactor(live-poll): remove unessesary code

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 export async function route(): Promise<Router> {
     const router = Router();
-    console.log(1);
     router.use((await import('./api')).router);
     return router;
 }
